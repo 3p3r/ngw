681ff7cb60e750a466d687e9e2d610b324665e6f
Sepehr Laal <sepehr.laal@gmail.com>
2016-02-24 16:49:15 -0800

    checked in base source files

---
+++ bindings/ngw.h
@@ -0,0 +1,13 @@
+#pragma once
+
+#ifdef __cplusplus
+extern "C" {
+#endif // __cplusplus
+
+typedef struct _Player		NgwPlayer;
+typedef struct _Listener	NgwListener;
+typedef struct _Renderer	NgwRenderer;
+
+#ifdef __cplusplus
+} // extern "C"
+#endif // __cplusplus
