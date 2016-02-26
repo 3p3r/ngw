a310f62827d78201c3ea14cee39e824d21099468
Sepehr Laal <sepehr.laal@gmail.com>
2016-02-26 15:35:50 -0800

    Adding more C API bindings

--- bindings/ngw.h
+++ bindings/ngw.h
@@ -7,8 +7,12 @@ extern "C" {
 typedef struct _Player     Player;
 typedef struct _Discoverer Discoverer;
 
-void ngw_add_plugin_path(const char* path);
-void ngw_add_binary_path(const char* path);
+void        ngw_add_plugin_path(const char* path);
+void        ngw_add_binary_path(const char* path);
+Player*     ngw_player_make(void);
+void        ngw_player_free(Player* player);
+Discoverer* ngw_discoverer_make(void);
+void        ngw_discoverer_free(Discoverer* discoverer);
 
 #ifdef __cplusplus
 } // extern "C"
--- bindings/ngw.h.cpp
+++ bindings/ngw.h.cpp
@@ -1,4 +1,8 @@
+#include "ngw.h"
 #include "ngw.hpp"
+
+struct _Player      final : public ngw::Player { };
+struct _Discoverer  final : public ngw::Discoverer { };
 
 #ifdef __cplusplus
 extern "C" {
@@ -10,6 +14,22 @@ void ngw_add_plugin_path(const char* path) {
 
 void ngw_add_binary_path(const char* path) {
     ngw::addBinaryPath(path);
+}
+
+Player* ngw_player_make(void) {
+    return new Player();
+}
+
+void ngw_player_free(Player* player) {
+    delete player;
+}
+
+Discoverer* ngw_discoverer_make(void) {
+    return new Discoverer();
+}
+
+void ngw_discoverer_free(Discoverer* discoverer) {
+    delete discoverer;
 }
 
 #ifdef __cplusplus
