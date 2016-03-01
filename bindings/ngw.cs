f694bba59cde5e243f10a7051b1833a707cfec3a
Sepehr Laal <sepehr.laal@gmail.com>
2016-02-29 21:46:49 -0800

    some logistics fixed

--- bindings/ngw.cs
+++ bindings/ngw.cs
@@ -7,18 +7,24 @@ namespace ngw
     {
         public enum State
         {
-            Pending,
-            Null,
-            Ready,
-            Paused,
-            Playing
+            Pending         = 0,
+            Null            = 1,
+            Ready           = 2,
+            Paused          = 3,
+            Playing         = 4
         }
 
-        IntPtr m_NativePlayer   = IntPtr.Zero;
+        public enum BufferType
+        {
+            BYTE_POINTER    = 0,
+            OPENGL_TEXTURE  = 1
+        }
 
-        GCHandle                m_ErrorCallbackHandle;
-        GCHandle                m_StateCallbackHandle;
-        GCHandle                m_StEndCallbackHandle;
+        IntPtr mNativePlayer   = IntPtr.Zero;
+
+        GCHandle                mErrorCallbackHandle;
+        GCHandle                mStateCallbackHandle;
+        GCHandle                mStEndCallbackHandle;
 
         public Action<string>   OnErrorReceived;
         public Action<State>    OnStateChanged;
@@ -26,135 +32,130 @@ namespace ngw
 
         public Player()
         {
-            m_NativePlayer = NativeMethods.ngw_player_make();
+            mNativePlayer = NativeMethods.ngw_player_make();
 
-            var error_delegate = new NativeMethods.ERROR_CALLBACK_TYPE((msg, player)=> {
+            var error_delegate = new NativeMethods.ErrorDelegate((msg, player)=> {
                 if (OnErrorReceived != null)
                     OnErrorReceived(msg);
             });
 
-            var state_delegate = new NativeMethods.STATE_CALLBACK_TYPE((old, player)=> {
+            var state_delegate = new NativeMethods.StateDelegate((old, player)=> {
                 if (OnStateChanged != null)
                     OnStateChanged(old);
             });
 
-            var stend_delegate = new NativeMethods.STREAM_END_CALLBACK_TYPE((player)=> {
+            var stend_delegate = new NativeMethods.StreamEndDelegate((player)=> {
                 if (OnStreamEnded != null)
                     OnStreamEnded();
             });
 
-            m_ErrorCallbackHandle = GCHandle.Alloc(error_delegate);
-            m_StateCallbackHandle = GCHandle.Alloc(state_delegate);
-            m_StEndCallbackHandle = GCHandle.Alloc(stend_delegate);
+            mErrorCallbackHandle = GCHandle.Alloc(error_delegate);
+            mStateCallbackHandle = GCHandle.Alloc(state_delegate);
+            mStEndCallbackHandle = GCHandle.Alloc(stend_delegate);
 
-            NativeMethods.ngw_player_set_error_callback(m_NativePlayer, error_delegate);
-            NativeMethods.ngw_player_set_state_callback(m_NativePlayer, state_delegate);
-            NativeMethods.ngw_player_set_stream_end_callback(m_NativePlayer, stend_delegate);
+            NativeMethods.ngw_player_set_error_callback(mNativePlayer, error_delegate);
+            NativeMethods.ngw_player_set_state_callback(mNativePlayer, state_delegate);
+            NativeMethods.ngw_player_set_stream_end_callback(mNativePlayer, stend_delegate);
         }
 
         public bool open(string path)
         {
-            return NativeMethods.ngw_player_open(m_NativePlayer, path);
+            return NativeMethods.ngw_player_open(mNativePlayer, path);
         }
 
         public bool open(string path, string format)
         {
-            return NativeMethods.ngw_player_open_format(m_NativePlayer, path, format);
+            return NativeMethods.ngw_player_open_format(mNativePlayer, path, format);
         }
 
         public bool open(string path, int width, int height)
         {
-            return NativeMethods.ngw_player_open_resize(m_NativePlayer, path, width, height);
+            return NativeMethods.ngw_player_open_resize(mNativePlayer, path, width, height);
         }
 
         public bool open(string path, int width, int height, string format)
         {
-            return NativeMethods.ngw_player_open_resize_format(m_NativePlayer, path, width, height, format);
+            return NativeMethods.ngw_player_open_resize_format(mNativePlayer, path, width, height, format);
         }
 
-        public void setBuffer(IntPtr pinned_byte_pointer)
+        public void setFrameBuffer(IntPtr pinned_frame_buffer, BufferType type)
         {
-            NativeMethods.ngw_player_set_sample_buffer(m_NativePlayer, pinned_byte_pointer, NativeMethods.NgwBuffer.BYTE_POINTER);
-        }
-
-        public void setBuffer(uint opengl_texture_name)
-        {
-            NativeMethods.ngw_player_set_sample_buffer(m_NativePlayer, (IntPtr)opengl_texture_name, NativeMethods.NgwBuffer.OPENGL_TEXTURE);
+            NativeMethods.ngw_player_set_frame_buffer(mNativePlayer, pinned_frame_buffer, type);
         }
 
         public int width
         {
-            get { return NativeMethods.ngw_player_get_width(m_NativePlayer); }
+            get { return NativeMethods.ngw_player_get_width(mNativePlayer); }
         }
 
         public int height
         {
-            get { return NativeMethods.ngw_player_get_height(m_NativePlayer); }
+            get { return NativeMethods.ngw_player_get_height(mNativePlayer); }
         }
 
         public State state
         {
-            get { return NativeMethods.ngw_player_get_state(m_NativePlayer); }
-            set { NativeMethods.ngw_player_set_state(m_NativePlayer, value); }
+            get { return NativeMethods.ngw_player_get_state(mNativePlayer); }
+            set { NativeMethods.ngw_player_set_state(mNativePlayer, value); }
         }
 
-        public bool Mute
+        public bool mute
         {
-            get { return NativeMethods.ngw_player_get_mute(m_NativePlayer); }
-            set { NativeMethods.ngw_player_set_mute(m_NativePlayer, value ? NativeMethods.NgwBool.TRUE : NativeMethods.NgwBool.FALSE); }
+            get { return NativeMethods.ngw_player_get_mute(mNativePlayer); }
+            set { NativeMethods.ngw_player_set_mute(mNativePlayer, value); }
         }
 
-        public bool Loop
+        public bool loop
         {
-            get { return NativeMethods.ngw_player_get_loop(m_NativePlayer); }
-            set { NativeMethods.ngw_player_set_loop(m_NativePlayer, value ? NativeMethods.NgwBool.TRUE : NativeMethods.NgwBool.FALSE); }
+            get { return NativeMethods.ngw_player_get_loop(mNativePlayer); }
+            set { NativeMethods.ngw_player_set_loop(mNativePlayer, value); }
         }
 
-        public double Volume
+        public double volume
         {
-            get { return NativeMethods.ngw_player_get_volume(m_NativePlayer); }
-            set { NativeMethods.ngw_player_set_volume(m_NativePlayer, value); }
+            get { return NativeMethods.ngw_player_get_volume(mNativePlayer); }
+            set { NativeMethods.ngw_player_set_volume(mNativePlayer, value); }
         }
 
-        public double Time
+        public double time
         {
-            get { return NativeMethods.ngw_player_get_time(m_NativePlayer); }
-            set { NativeMethods.ngw_player_set_time(m_NativePlayer, value); }
+            get { return NativeMethods.ngw_player_get_time(mNativePlayer); }
+            set { NativeMethods.ngw_player_set_time(mNativePlayer, value); }
         }
 
-        public double Rate
+        public double rate
         {
-            get { return NativeMethods.ngw_player_get_rate(m_NativePlayer); }
-            set { NativeMethods.ngw_player_set_rate(m_NativePlayer, value); }
+            get { return NativeMethods.ngw_player_get_rate(mNativePlayer); }
+            set { NativeMethods.ngw_player_set_rate(mNativePlayer, value); }
         }
 
-        public double Duration
+        public double duration
         {
-            get { return NativeMethods.ngw_player_get_duration(m_NativePlayer); }
+            get { return NativeMethods.ngw_player_get_duration(mNativePlayer); }
         }
 
-        public void play() { NativeMethods.ngw_player_play(m_NativePlayer); }
-        public void stop() { NativeMethods.ngw_player_stop(m_NativePlayer); }
-        public void pause() { NativeMethods.ngw_player_pause(m_NativePlayer); }
-        public void replay() { NativeMethods.ngw_player_replay(m_NativePlayer); }
-        public void update() { NativeMethods.ngw_player_update(m_NativePlayer); }
+        public void play() { NativeMethods.ngw_player_play(mNativePlayer); }
+        public void stop() { NativeMethods.ngw_player_stop(mNativePlayer); }
+        public void pause() { NativeMethods.ngw_player_pause(mNativePlayer); }
+        public void replay() { NativeMethods.ngw_player_replay(mNativePlayer); }
+        public void update() { NativeMethods.ngw_player_update(mNativePlayer); }
 
         #region IDisposable Support
-        bool m_DisposedValue = false;
+        bool mDisposedValue = false;
 
         protected virtual void Dispose(bool disposing)
         {
-            if (!m_DisposedValue)
+            if (!mDisposedValue)
             {
-                NativeMethods.ngw_player_free(m_NativePlayer);
-                m_NativePlayer = IntPtr.Zero;
-                m_DisposedValue = true;
+                NativeMethods.ngw_player_free(mNativePlayer);
+                mNativePlayer = IntPtr.Zero;
+                mDisposedValue = true;
 
                 if (disposing)
                 {
-                    m_ErrorCallbackHandle.Free();
-                    m_StateCallbackHandle.Free();
-                    m_StEndCallbackHandle.Free();
+                    mErrorCallbackHandle.Free();
+                    mStateCallbackHandle.Free();
+                    mStEndCallbackHandle.Free();
                 }
             }
         }
@@ -174,78 +175,78 @@ namespace ngw
 
     public class Discoverer : IDisposable
     {
-        IntPtr m_NativeDiscoverer = IntPtr.Zero;
+        IntPtr mNativeDiscoverer = IntPtr.Zero;
 
         public Discoverer()
         {
-            m_NativeDiscoverer = NativeMethods.ngw_discoverer_make();
+            mNativeDiscoverer = NativeMethods.ngw_discoverer_make();
         }
 
         public bool open(string path)
         {
-            return NativeMethods.ngw_discoverer_open(m_NativeDiscoverer, path);
+            return NativeMethods.ngw_discoverer_open(mNativeDiscoverer, path);
         }
 
         public int width
         {
-            get { return NativeMethods.ngw_discoverer_get_width(m_NativeDiscoverer); }
+            get { return NativeMethods.ngw_discoverer_get_width(mNativeDiscoverer); }
         }
 
         public int height
         {
-            get { return NativeMethods.ngw_discoverer_get_height(m_NativeDiscoverer); }
+            get { return NativeMethods.ngw_discoverer_get_height(mNativeDiscoverer); }
         }
 
         public bool seekable
         {
-            get { return NativeMethods.ngw_discoverer_get_seekable(m_NativeDiscoverer); }
+            get { return NativeMethods.ngw_discoverer_get_seekable(mNativeDiscoverer); }
         }
 
         public bool hasAudio
         {
-            get { return NativeMethods.ngw_discoverer_get_has_audio(m_NativeDiscoverer); }
+            get { return NativeMethods.ngw_discoverer_get_has_audio(mNativeDiscoverer); }
         }
 
         public bool hasVideo
         {
-            get { return NativeMethods.ngw_discoverer_get_has_video(m_NativeDiscoverer); }
+            get { return NativeMethods.ngw_discoverer_get_has_video(mNativeDiscoverer); }
         }
 
         public float frameRate
         {
-            get { return NativeMethods.ngw_discoverer_get_framerate(m_NativeDiscoverer); }
+            get { return NativeMethods.ngw_discoverer_get_framerate(mNativeDiscoverer); }
         }
 
         public double duration
         {
-            get { return NativeMethods.ngw_discoverer_get_duration(m_NativeDiscoverer); }
+            get { return NativeMethods.ngw_discoverer_get_duration(mNativeDiscoverer); }
         }
 
         public uint sampleRate
         {
-            get { return NativeMethods.ngw_discoverer_get_sample_rate(m_NativeDiscoverer); }
+            get { return NativeMethods.ngw_discoverer_get_sample_rate(mNativeDiscoverer); }
         }
 
         public uint bitRate
         {
-            get { return NativeMethods.ngw_discoverer_get_bit_rate(m_NativeDiscoverer); }
+            get { return NativeMethods.ngw_discoverer_get_bit_rate(mNativeDiscoverer); }
         }
 
         #region IDisposable Support
-        bool m_DisposedValue = false;
+        bool mDisposedValue = false;
 
         protected virtual void Dispose(bool disposing)
         {
-            if (!m_DisposedValue)
+            if (!mDisposedValue)
             {
                 if (disposing)
                 {
                     // no managed resources to be freed.
                 }
 
-                NativeMethods.ngw_discoverer_free(m_NativeDiscoverer);
-                m_NativeDiscoverer = IntPtr.Zero;
-                m_DisposedValue = true;
+                NativeMethods.ngw_discoverer_free(mNativeDiscoverer);
+                mNativeDiscoverer = IntPtr.Zero;
+                mDisposedValue = true;
             }
         }
 
@@ -264,22 +265,9 @@ namespace ngw
 
     internal static class NativeMethods
     {
-        public enum NgwBool
-        {
-            FALSE               = 0,
-            TRUE                = 1
-        }
-
-        public enum NgwBuffer
-        {
-            BYTE_POINTER        = 0,
-            OPENGL_TEXTURE      = 1,
-            CALLBACK_FUNCTION   = 2
-        }
-
-        public delegate void ERROR_CALLBACK_TYPE(string message, IntPtr player);
-        public delegate void STATE_CALLBACK_TYPE(Player.State state, IntPtr player);
-        public delegate void STREAM_END_CALLBACK_TYPE(IntPtr player);
+        public delegate void ErrorDelegate(string message, IntPtr player);
+        public delegate void StateDelegate(Player.State state, IntPtr player);
+        public delegate void StreamEndDelegate(IntPtr player);
 
         [DllImport("ngw")]
         public static extern void ngw_add_plugin_path(string path);
@@ -337,7 +325,7 @@ namespace ngw
         public static extern double ngw_player_get_duration(IntPtr player);
 
         [DllImport("ngw")]
-        public static extern void ngw_player_set_loop(IntPtr player, NgwBool on);
+        public static extern void ngw_player_set_loop(IntPtr player, [MarshalAs(UnmanagedType.Bool)] bool on);
 
         [DllImport("ngw")]
         [return: MarshalAs(UnmanagedType.Bool)]
@@ -356,7 +344,7 @@ namespace ngw
         public static extern double ngw_player_get_volume(IntPtr player);
 
         [DllImport("ngw")]
-        public static extern void ngw_player_set_mute(IntPtr player, NgwBool on);
+        public static extern void ngw_player_set_mute(IntPtr player, [MarshalAs(UnmanagedType.Bool)] bool on);
 
         [DllImport("ngw")]
         [return: MarshalAs(UnmanagedType.Bool)]
@@ -378,16 +366,16 @@ namespace ngw
         public static extern IntPtr ngw_player_get_user_data(IntPtr player);
 
         [DllImport("ngw")]
-        public static extern void ngw_player_set_sample_buffer(IntPtr player, IntPtr buffer, NgwBuffer type);
+        public static extern void ngw_player_set_frame_buffer(IntPtr player, IntPtr buffer, Player.BufferType type);
 
         [DllImport("ngw")]
-        public static extern void ngw_player_set_error_callback(IntPtr player, ERROR_CALLBACK_TYPE cb);
+        public static extern void ngw_player_set_error_callback(IntPtr player, ErrorDelegate cb);
 
         [DllImport("ngw")]
-        public static extern void ngw_player_set_state_callback(IntPtr player, STATE_CALLBACK_TYPE cb);
+        public static extern void ngw_player_set_state_callback(IntPtr player, StateDelegate cb);
 
         [DllImport("ngw")]
-        public static extern void ngw_player_set_stream_end_callback(IntPtr player, STREAM_END_CALLBACK_TYPE cb);
+        public static extern void ngw_player_set_stream_end_callback(IntPtr player, StreamEndDelegate cb);
 
         [DllImport("ngw")]
         public static extern double ngw_player_get_rate(IntPtr player);
