1542b42ca5b2d28776f0021c858eebf449fd17fc
Sepehr Laal <sepehr.laal@gmail.com>
2016-02-29 21:46:38 -0800

    started working on MonoBehaviours

---
+++ samples/unity/NgwUnitySample/Assets/ngw/MediaPlayer.cs
@@ -0,0 +1,24 @@
+﻿namespace ngw
+{
+    using UnityEngine;
+
+    public class MediaPlayer : MonoBehaviour
+    {
+        Player mPlayer;
+
+        void Start()
+        {
+            mPlayer = new Player();
+        }
+
+        void Update()
+        {
+            mPlayer.update();
+        }
+
+        void OnDestroy()
+        {
+            mPlayer.Dispose();
+        }
+    }
+}
